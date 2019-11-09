30d75f607569f23ee4ad0f5e2422bcbd7a9285ec
Randy Steele <steelerx155@gmail.com>
2019-11-09 22:23:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,12 +24,8 @@ class Song
   end
   
   def artist_name(name)
-   if find_or_create_by(name)
-      binding.pry
-      find_or_create_by(name)
       
-    else
-      self.new(artist_name)
+    
   end    
 end
 
