62b4de6f78dac05d5408e64aadca13901efcb780
Randy Steele <steelerx155@gmail.com>
2019-11-09 21:55:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   
   def self.new_by_filename(filename)
     split_files = filename.split(" - ")
-    binding.pry
+    # binding.pry
     song_name = split_files[1]
     new_song = self.new(song_name)
     new_song.artist = Artist.new(split_files[0])
