508323a7d65902d115f53cce4f4d2f19f5e9a9a6
Randy Steele <steelerx155@gmail.com>
2019-11-09 20:18:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Song
   
-  attr_accessor :name, :artist, :artist_name
+  attr_accessor :name, :artist
   
   @@all = []
   
@@ -15,15 +15,17 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    split_files = filename.split( " - " )
+    split_files = filename.split(" - ")
+    # binding.pry
     song_name = split_files[1]
     new_song = self.new(song_name)
-    
+    new_song.artist = Artist.new(split_files[0])
+    new_song
   end
   
   def artist_name(name)
     self.all.find {| artist | artist.name == name}
-    
+     
     
   end
 
