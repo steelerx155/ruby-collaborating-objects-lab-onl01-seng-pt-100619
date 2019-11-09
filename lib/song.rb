2e0888a4a3274ed30bb647c8d5bf16d3406efa46
Randy Steele <steelerx155@gmail.com>
2019-11-09 22:43:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,3 +29,15 @@ class Song
     
   end
 end  
+
+
+
+
+#artist_name=
+    accepts an artist's name, finds or creates an Artist instance and assigns it to the Song's artist attribute (FAILED - 1)
+
+Failures:
+
+  1) Song #artist_name= accepts an artist's name, finds or creates an Artist instance and assigns it to the Song's artist attribute
+     Failure/Error: expect(song.artist).to be_an(Artist)
+       expected nil to be a kind of Artist
